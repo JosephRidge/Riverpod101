53644a24bd82489f4014aec9571feb2a2828a44b
JosephRidge <josephridge18@gmail.com>
2025-08-22 11:45:29 +0300

    updated

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -6,6 +6,48 @@ final productNameProvider = Provider<String>((ref) => "Loaf");
 final counterProvider = Provider<int>((ref) => 0);
 final numbersProvider = Provider<List<int>>((ref) => [0, 1, 2, 3]);
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.amber),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Hellow World", style: TextStyle(fontSize: 32)),
+            Consumer(
+              builder: (context, ref, child) {
+                final name = ref.read(productNameProvider);
+                return Text("Hellow Mr. $name 2.0", style: TextStyle(fontSize: 32));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+class TestWidget extends ConsumerWidget {
+  const TestWidget({super.key});
+ 
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Placeholder();
+  }
+}
+*/
+
+/*
 // widget
 class MyHomePage extends ConsumerStatefulWidget {
   // ref
@@ -46,3 +88,5 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
     );
   }
 }
+
+*/
